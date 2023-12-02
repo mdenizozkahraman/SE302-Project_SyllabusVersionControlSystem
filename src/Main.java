a1bde0601e299560a5922e871a232670215f2958
yigitcam1 <yiggit.cam@gmail.com>
2023-12-02 12:13:32 +0300

    yigit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Test Mehmet Deniz Ozkahraman");
+        System.out.println("YİĞİT ÇAM");
+
+
     }
 }
