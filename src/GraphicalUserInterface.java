4015f547119a6a04feeb65a999c7c1e0d4e1973a
Cerenengin15 <105562497+Cerenengin15@users.noreply.github.com>
2023-12-09 14:07:18 +0300

    Adaptable for full screen mode

--- src/GraphicalUserInterface.java
+++ src/GraphicalUserInterface.java
@@ -1,9 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
+import java.awt.event.*;
 
 public class GraphicalUserInterface {
     public static void main(String[] args) {
@@ -31,6 +28,13 @@ public class GraphicalUserInterface {
         f.add(benjamin5);
         f.add(searchBar);
 
+        JLabel currentVersion = new JLabel("Current Syllabus");
+        currentVersion.setHorizontalAlignment(SwingConstants.CENTER);
+        currentVersion.setVerticalAlignment(SwingConstants.CENTER);
+        currentVersion.setBounds(100, 50, 500, 600);
+        currentVersion.setBorder(BorderFactory.createEtchedBorder());
+        f.add(currentVersion, BorderLayout.CENTER);
+
         f.addComponentListener(new ComponentAdapter() {
             public void componentResized(ComponentEvent e) {
                 int width = f.getContentPane().getWidth();
@@ -43,65 +47,21 @@ public class GraphicalUserInterface {
                 benjamin5.setBounds(width - 150, 190, 100, 50);
             }
         });
-        JLabel currentVersion = new JLabel("Current Syllabus");
-        currentVersion.setHorizontalAlignment(SwingConstants.CENTER);
-        currentVersion.setVerticalAlignment(SwingConstants.CENTER);
 
-        currentVersion.setBounds(100, 50, 500, 600);
-        currentVersion.setBorder(BorderFactory.createEtchedBorder());
+        f.addWindowStateListener(new WindowAdapter() {
+            public void windowStateChanged(WindowEvent e) {
+                if ((e.getNewState() & JFrame.MAXIMIZED_BOTH) == JFrame.MAXIMIZED_BOTH) {
+                    int width = f.getContentPane().getWidth();
+                    int height = f.getContentPane().getHeight();
 
-
-        f.add(currentVersion, BorderLayout.CENTER);
-
-        benjamin5.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                JFrame previousVersionsFrame = new JFrame("Previous Versions");
-                JButton btn1, btn2;
-                btn1 = new JButton("Next");
-                btn2 = new JButton("Previous");
-
-                previousVersionsFrame.add(btn1);
-                previousVersionsFrame.add(btn2);
-
-
-                previousVersionsFrame.addComponentListener(new ComponentAdapter() {
-                    public void componentResized(ComponentEvent e) {
-                        int width = previousVersionsFrame.getContentPane().getWidth();
-                        int height = previousVersionsFrame.getContentPane().getHeight();
-
-                        btn1.setBounds( width - 150, 480, 100, 25);
-                        btn2.setBounds(50,   480, 100, 25);
-
-                        int labelWidth = (width - 150) / 2; // Genişlik ayarı
-                        int labelHeight = height - 100; // Yükseklik ayarı
-
-                        JLabel previousVersionsLabel1 = new JLabel("Former Version");
-                        previousVersionsLabel1.setBorder(BorderFactory.createEtchedBorder());
-
-                        JLabel previousVersionsLabel2 = new JLabel("Current Version");
-                        previousVersionsLabel2.setBorder(BorderFactory.createEtchedBorder());
-
-                        JPanel panel = new JPanel(new GridLayout(1, 2));
-                        panel.setBounds(50, 50, width - 100, height - 100);
-                        panel.add(previousVersionsLabel1);
-                        panel.add(previousVersionsLabel2);
-
-                        previousVersionsFrame.add(panel);
-                    }
-
-                });
-
-                previousVersionsFrame.setSize(600, 550);
-                previousVersionsFrame.setLayout(null);
-                previousVersionsFrame.setVisible(true);
+                    currentVersion.setBounds(100, 50, width - 200, height - 100);
+                }
             }
         });
 
-        f.setSize(800, 725);
+        f.setExtendedState(JFrame.MAXIMIZED_BOTH);
         f.setLayout(null);
         f.setVisible(true);
-        f.setLocationRelativeTo(null);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
-
