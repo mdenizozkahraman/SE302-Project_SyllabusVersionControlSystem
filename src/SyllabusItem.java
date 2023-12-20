6f732277a007d27e5e53930b3a744f9b3aa599ce
Cerenengin15 <105562497+Cerenengin15@users.noreply.github.com>
2023-12-20 22:54:13 +0300

    delete button+edit and save button vol1

--- src/SyllabusItem.java
+++ src/SyllabusItem.java
@@ -2,10 +2,14 @@ public class SyllabusItem {
     private final Syllabus syllabus;
 
     public SyllabusItem(Syllabus syllabus) {
+
         this.syllabus = syllabus;
+
     }
 
     public Syllabus getSyllabus() {
         return syllabus;
     }
+
+
 }
